675ef52922a6a9b07e180cdd56d807b9985c23f2
Християн Христов <hh13147454@edu.mon.bg>
2024-11-14 20:09:43 +0200

    1vi

---
+++ F1World2/EmailSender.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Identity.UI.Services;
+using System.Net;
+using System.Net.Mail;
+using System.Threading.Tasks;
+
+public class EmailSender : IEmailSender
+{
+    private readonly IConfiguration _configuration;
+
+    public EmailSender(IConfiguration configuration)
+    {
+        _configuration = configuration;
+    }
+
+    public async Task SendEmailAsync(string email, string subject, string htmlMessage)
+    {
+        var smtpClient = new SmtpClient
+        {
+            Host = _configuration["Email:SmtpHost"],       // Примерно "smtp.gmail.com"
+            Port = int.Parse(_configuration["Email:SmtpPort"]),
+            EnableSsl = true,
+            Credentials = new NetworkCredential(
+                _configuration["Email:SmtpUser"],
+                _configuration["Email:SmtpPass"])
+        };
+
+        var mailMessage = new MailMessage
+        {
+            From = new MailAddress(_configuration["Email:FromAddress"]),
+            Subject = subject,
+            Body = htmlMessage,
+            IsBodyHtml = true,
+        };
+        mailMessage.To.Add(email);
+    }
+}
--- F1World2/Program.cs
+++ F1World2/Program.cs
@@ -1,9 +1,10 @@
 using F1World2.Data;
 using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI.Services;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
-
+builder.Services.AddTransient<IEmailSender, EmailSender>();
 // Add services to the container.
 var connectionString = builder.Configuration.GetConnectionString("DefaultConnection") ?? throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
